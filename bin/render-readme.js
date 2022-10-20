6b7f217fa190902ed9743d35788d30f6599092f3
yemiancheng <ymc.github@gmail.com>
2022-10-20 22:39:27 +0800

    chore(core): update script

--- bin/render-readme.js
+++ bin/render-readme.js
@@ -1,11 +1,155 @@
 #!/usr/bin/env node
 
-import { createReadStream, createWriteStream } from "fs";
+import formatDate from "../lib/format-date.js";
+import { JsonStream } from "../lib/json-stream-io.js";
+import { TextStream } from "../lib/text-stream-io.js";
+import { writeTpl } from "../lib/render-tpl.js";
 
 const { log } = console;
+
+function getTableBody(item, option = {}) {
+    let { full_name, description, languages } = item;
+
+    let res;
+    // res = `|[${full_name}](https://github.com/${full_name})|${description}|`;
+    let headTpl;
+    res = `[{full_name}](https://github.com/{full_name})|{description}`;
+    if (
+        option.useStarredTime &&
+        (!option.topicBy || option.topicBy !== "yearmonth")
+    ) {
+        res = `${res}|{starred_at}`;
+    }
+    if (option.topicBy == "yearmonth") {
+        res = `${res}|{languages}`;
+    }
+    res = writeTpl(res, item);
+
+    return res;
+}
+function getTableHeader(option) {
+    let res;
+    let head = `repo,description`;
+    if (
+        option.useStarredTime &&
+        (!option.topicBy || option.topicBy !== "yearmonth")
+    ) {
+        head = `${head},starred_at`;
+    }
+
+    if (option.topicBy == "yearmonth") {
+        head = `${head},language`;
+    }
+
+    head = head.replace(/,/gi, "|");
+    res = [
+        head,
+        head
+            .split("|")
+            .map((v) => ":--")
+            .join("|"),
+    ].join("\n");
+    // log(res)
+    return res;
+}
+
+function table(title, body, head) {
+    //[md:set table collum width](https://github.com/markedjs/marked/issues/266)
+    //https://9to5answer.com/set-table-column-width-via-markdown
+    let res = `
+## ${title}
+${head}
+${body}
+`;
+    return res;
+}
+
+function getTabBody(item, option = {}) {
+    let { full_name, description, languages, starred_at } = item;
+
+    let res;
+    // res= `- [${full_name}](https://github.com/${full_name}) - ${description}\n`;
+    res = `-`;
+    if (
+        option.useStarredTime &&
+        (!option.topicBy || option.topicBy !== "yearmonth")
+    ) {
+        res = writeTpl(`${res} {starred_at}`, item);
+    }
+    if (option.topicBy == "yearmonth") {
+        res = `${res} {languages}`;
+    }
+
+    res = writeTpl(
+        `${res} [{full_name}](https://github.com/{full_name}) - {description}`,
+        item
+    );
+    res = `${res}\n`;
+    return res;
+}
+function tab(title, body) {
+    let res = `
+## ${title}
+${body}
+`;
+    return res;
+}
+
+function render(allstars, option = {}) {
+    let res;
+    //
+    let topicBy = option.topicBy;
+    if (!topicBy) topicBy = "languages";
+    let allLanguages = allstars.map((item) => item[topicBy]);
+    log(`[info] delete dup topic before to render it`);
+    allLanguages = [...new Set(allLanguages)];
+
+    if (topicBy == "yearmonth") {
+        log(`[info] sort title time`);
+        allLanguages = allLanguages.sort((a, b) => {
+            // log(b - a)//NaN
+            // log(new Date(b)- new Date(a))
+            return new Date(b) - new Date(a);
+        });
+        // log(allLanguages)
+    }
+
+    res = allLanguages
+        .map((language) => {
+            let part;
+            part = allstars
+                .filter((v) => {
+                    return v[topicBy] == language;
+                })
+                .map((item) => {
+                    switch (option.style) {
+                        case "tab":
+                            return getTabBody(item, option);
+                            break;
+
+                        default:
+                            return getTableBody(item, option);
+                            break;
+                    }
+                })
+                .join(`\n`);
+
+            if (!language) language = "unknow";
+            switch (option.style) {
+                case "tab":
+                    return tab(language, part);
+                default:
+                    return table(language, part, getTableHeader(option));
+            }
+            // return table(language, part);
+        })
+        .join("\n\n");
+    return res;
+}
+
 async function main() {
     const jsonstream = new JsonStream();
-    const textstream = new TextFileIO();
+    const textstream = new TextStream();
 
     let fileopt = {
         dataLoc: `stars-list-shim.all.json`,
@@ -16,76 +160,56 @@ async function main() {
     jsonstream.init(`${fileopt.dataLoc}`);
     allstars = await jsonstream.read([]);
 
-    function getTableBody(item) {
-        let { full_name, description, languages } = item;
-
-        // if(language==language) language='unknow'
-        let res = `|[${full_name}](https://github.com/${full_name})|${description}|`;
-        return res;
-    }
-    function table(title, body) {
-        let res = `
-## ${title}
-repo|description|
-:----|:----|
-${body}
-`;
-        return res;
+    log(`[info] sort allstars by starrd time`);
+    if (allstars[0].starred_at) {
+        allstars = allstars.sort((a, b) => {
+            // log(b.starred_at - a.starred_at)//NaN
+            return new Date(b.starred_at) - new Date(a.starred_at);
+        });
     }
 
-    function getTabBody(item) {
-        let { full_name, description, languages } = item;
-
-        // if(language==language) language='unknow'
-        let res = `- [${full_name}](https://github.com/${full_name}) - ${description}\n`;
-        return res;
-    }
-    function tab(title, body) {
-        let res = `
-## ${title}
-${body}
-`;
-        return res;
-    }
+    log(`[info] format starrd time`);
+    // "yyyy-MM-dd HH:mm:ss"
+    allstars = allstars.map((item) => {
+        let { starred_at } = item;
+        if (starred_at) {
+            item.starred_at = formatDate("yyyy-MM-dd", new Date(starred_at));
+            item.yearmonth = formatDate("yyyy-MM", new Date(starred_at));
+        } else {
+            item.starred_at = "";
+            item.yearmonth = "";
+        }
+        return item;
+    });
 
     let style;
-    style='tab'
-    log(`[task] make readme table`);
-    let allLanguages = allstars.map((item) => item.languages);
-    log(`[info] delete dup language before to readme table`);
-    allLanguages = [...new Set(allLanguages)];
-    allstars = allLanguages
-        .map((language) => {
-            let part;
-            part = allstars
-                .filter((v) => {
-                    return v.languages == language;
-                })
-                .map((item) => {
-                    switch (style) {
-                        case "tab":
-                            return getTabBody(item);
-                            break;
-
-                        default:
-                            return getTableBody(item);
-                            break;
-                    }
-                })
-                .join(`\n`);
-
-            switch (style) {
-                case "tab":
-                    return tab(language, part);
-                default:
-                    return table(language, part);
-            }
-            // return table(language, part);
-        })
-        .join("\n\n");
+    // style = "tab";
+    // log(`[task] make readme table`);
+    log(`[task] render allstars to markdown`);
+    let renderOption = {
+        style: "tab", //tab
+        useStarredTime: true,
+        topicBy: "", //yearmonth
+    };
+    allstars = render(allstars, renderOption);
     allstars = allstars.trim();
+    let tablestyle = `
+<style>
+table{
+    display:table;
+    width:100%;
+}
+table th:nth-of-type(1),table th:nth-of-type(3) {
+    width:10%;
+}
+</style>
+`;
+
+    if (!renderOption.style) allstars = `${tablestyle}\n\n${allstars}`;
+
     // log(allstars)
 
+    log(`[task] add front content`);
     textstream.init(`template/readme.head.md`);
     let front = await textstream.read("");
     if (front) {
@@ -98,227 +222,11 @@ ${body}
 }
 
 // @ymc/streamio
-function readStream(stream) {
-    return new Promise((resolve, reject) => {
-        let data = "";
-        stream
-            .on("data", (chunk) => {
-                data += chunk.toString();
-            })
-            .on("end", () => {
-                resolve(data);
-            })
-            .on("error", reject);
-    });
-}
-function writeStream({ stream, data }) {
-    return new Promise((resolve, reject) => {
-        // write
-        stream.write(data, "utf-8");
-        // fire end
-        stream.end();
-        // desc-x-s: handle event finish and err
-        stream
-            .on("finish", () => {
-                resolve(data);
-            })
-            .on("error", reject);
-        // desc-x-e: handle event finish and err
-    });
-}
-// @ymc/text-streamio
-/**
- * @sample
- * ```
- * txtfileio.file.name="CHANGELO.md"
- * //or
- * txtfileio.init("CHANGELO.md")
- * await txtfileio.read()
- * txtfileio.option.writemode='overide'
- * await txtfileio.write('')
- * ```
- */
-class TextFileIO {
-    constructor(name = "CHANGELO.md") {
-        this.init(name);
-    }
-
-    /**
-     * read file async (stream mode)
-     * @param {string|undefined} def
-     * @returns {Prmosie<string>}
-     */
-    async read(def = "") {
-        const { file } = this;
-        let reader;
-        let res;
-        try {
-            reader = createReadStream(file.name);
-            res = await readStream(reader);
-        } catch (error) {
-            res = def;
-        }
-        file.data = res;
-        return res;
-    }
-
-    /**
-     * write file async (stream mode)
-     * @param {string} data
-     * @returns {Prmosie<void>}
-     */
-    async write(data) {
-        const { file, option } = this;
-        let writer;
-        let old;
-        writer = createWriteStream(file.name);
-        old = file.data;
-        // insert-head?append?override?
-        // let writemode = "override";
-        switch (option.writemode) {
-            case "override":
-                data = `${data}`;
-                break;
-            // case "head":
-            //   data = `${data}\n${old}`;
-            //   break;
-            case "append":
-                data = `${old}\n${data}`;
-                break;
-            // case "override":
-            //   data = `${data}`;
-            case "head":
-                data = `${data}\n${old}`;
-            default:
-                break;
-        }
-        file.data = data;
-        await writeStream({ stream: writer, data });
-    }
-
-    /**
-     *
-     * @param {string} name
-     * @param {string} data
-     * @returns {this}
-     */
-    init(name = "CHANGELO.md", data = "") {
-        this.file = {
-            name,
-            data,
-        };
-        this.option = {};
-        return this;
-    }
-
-    /**
-     * ceate a new instance
-     * @param  {...any} option
-     * @returns
-     */
-    new(...option) {
-        return new TextFileIO(...option);
-    }
-}
-
-// @ymc/json-streamio
-/**
- * @sample
- * ```
- * jsonstream.file.name="package.json"
- * //or
- * jsonstream.init("package.json")
- * await jsonstream.read()
- * await jsonstream.write({})
- * ```
- */
-class JsonStream {
-    constructor(name, data) {
-        this.init(name, data);
-    }
-
-    /**
-     * read file async (stream mode)
-     * @param {{}|[]} def
-     * @returns {Prmosie<json>}
-     */
-    async read(def = {}) {
-        const { file } = this;
-        let reader;
-        let res;
-        try {
-            reader = createReadStream(file.name);
-            res = await readStream(reader);
-            res = JSON.parse(res);
-        } catch (error) {
-            // console.log(error);
-            res = def;
-        }
-        file.data = res;
-        return res;
-    }
-
-    /**
-     * write file async (stream mode)
-     * @param {{}|[]|undefined} data
-     * @returns {Prmosie<void>}
-     */
-    async write(data) {
-        const { file, option } = this;
-        let writer;
-        try {
-            writer = createWriteStream(file.name);
-            if (data) {
-                file.data = data;
-            } else {
-                data = file.data;
-            }
-            await writeStream({
-                stream: writer,
-                data: JSON.stringify(data, null, 2),
-            });
-        } catch (error) {}
-    }
-
-    init(name = "package.json", data = {}) {
-        this.file = {
-            name,
-            data,
-        };
-        this.option = {};
-    }
-
-    new(...option) {
-        return new JsonStream(...option);
-    }
-}
-
-function renderTpl(tpl = "", menifest) {
-    let res = tpl;
-    let name;
-    let suffix;
-    Object.keys(menifest).forEach((key) => {
-        const value = menifest[key];
-        res = res.replace(new RegExp(`{${key}}`, "ig"), value);
-    });
-    return res;
-}
-/**
- *
- * @param {string} tpl
- * @param {{}} data
- * @returns {string|function({}):string}
- * @sample
- * ```
- * writeTpl('{method} repo/owner',{method:'POST'}) //POST repo/owner
- * ```
- */
-function writeTpl(tpl, data) {
-    if (data) {
-        return renderTpl(tpl, data);
-    }
-    return (v) => renderTpl(tpl, v);
-}
+// touch lib/stream-io.js
+// @ymc/text-stream-io
+// touch lib/text-stream-io.js
+// @ymc/json-stream-io
+// @ymc/render-tpl
 
 main();
 // bin/render-readme.js
